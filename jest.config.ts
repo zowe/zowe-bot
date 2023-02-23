f4a9ff9a3e111bf340c3e17e8be792df31bd83db
MarkAckert <mark.ackert@broadcom.com>
2023-02-23 15:46:49 -0500

    chore: init ci builds, using zowe/chat as basis
    Signed-off-by: MarkAckert <mark.ackert@broadcom.com>

---
+++ jest.config.ts
@@ -0,0 +1,45 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+
+// eslint-disable-next-line node/no-unpublished-import
+import type { Config } from 'jest';
+import * as fs from 'fs-extra';
+
+// jest-sonar will not create the fully qualified path to the results
+fs.mkdirpSync('__tests__/__results__/jest-sonar');
+
+// Jest 'projects' specifications do not inherit global properties by default, so use an object to track these.
+const commonProperties: Config = {
+  moduleFileExtensions: ['ts', 'js', 'tsx', 'jsx', 'json', 'node'],
+  modulePathIgnorePatterns: ['.*?__tests__/__snapshots__/.*', '.*/dist/.*', '.*/node_modules/.*', '.*/lib/.*'],
+  preset: 'ts-jest',
+  testRegex: '__tests__.*\\.*?\\.(spec|test)\\.ts$',
+};
+
+const config: Config = {
+  ...commonProperties,
+  coverageDirectory: '<rootDir>/__tests__/__results__/unit/coverage',
+  coverageReporters: ['json', 'lcov', 'text', 'cobertura'],
+  globals: {
+    'ts-jest': {
+      disableSourceMapSupport: true,
+    },
+  },
+  roots: ['<rootDir>'],
+  setupFilesAfterEnv: ['./__tests__/beforeTests.ts'],
+  testEnvironment: 'node',
+
+  testResultsProcessor: 'jest-sonar-reporter',
+  transform: {
+    '^.+\\.ts?$': 'ts-jest',
+  },
+};
+
+export default config;
